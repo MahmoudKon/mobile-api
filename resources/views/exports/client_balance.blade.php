327a69e020d2ce823d77255b40802baff9b3a2ba
MahmoudKon <mahmoud_mohammed050@yahoo.com>
2023-03-15 00:16:11 +0200

    add style for export file

--- resources/views/exports/client_balance.blade.php
+++ resources/views/exports/client_balance.blade.php
@@ -1,22 +1,22 @@
 <table class="table">
     <thead>
         <tr>
-            <th scope="col">م</th>
-            <th scope="col">التاريخ</th>
-            <th scope="col">صادر له</th>
-            <th scope="col">مدفوع منه</th>
-            <th scope="col">الرصيد</th>
-            <th scope="col">الرصيدالبيان</th>
+            <th scope="col" style="width: 50px;  text-align: center">م</th>
+            <th scope="col" style="width: 100px; text-align: center">التاريخ</th>
+            <th scope="col" style="width: 100px; text-align: center">صادر له</th>
+            <th scope="col" style="width: 100px; text-align: center">مدفوع منه</th>
+            <th scope="col" style="width: 100px; text-align: center">الرصيد</th>
+            <th scope="col" style="width: 200px; text-align: center">الرصيدالبيان</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($transactions as $transaction)
             <tr>
-                <td scope="col"> {{ $loop->iteration }} </td>
-                <td scope="col"> {{ $transaction->pay_day }} </td>
-                <td scope="col"> {{ $transaction->git }} </td>
-                <td scope="col"> {{ $transaction->pay }} </td>
-                <td scope="col"> {{ $transaction->balance }} </td>
+                <td scope="col" style="text-align: center"> {{ $loop->iteration }} </td>
+                <td scope="col" style="text-align: center"> {{ $transaction->pay_day }} </td>
+                <td scope="col" style="text-align: center"> {{ $transaction->git }} </td>
+                <td scope="col" style="text-align: center"> {{ $transaction->pay }} </td>
+                <td scope="col" style="text-align: center"> {{ $transaction->balance }} </td>
                 <td scope="col"> {{ $transaction->installment_id > 0 ? "سداد قسط" : $transaction->details }} </td>
             </tr>
         @endforeach
